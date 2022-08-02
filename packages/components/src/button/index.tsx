97c168d7b88e35ff565b22325b5f6d92cd43d96c
LuciferWZX <2396423791@qq.com>
2022-08-03 04:43:13 +0800

    dd

--- packages/components/src/button/index.tsx
+++ packages/components/src/button/index.tsx
@@ -4,7 +4,7 @@ interface ButtonProps{
     disabled?:boolean
 }
 const Button:FC<ButtonProps> = (props) => {
-    console.log("button121")
+    console.log("button1213")
     return(
         <div className={'btn'}> xxx{JSON.stringify(props.disabled)}</div>
     )
