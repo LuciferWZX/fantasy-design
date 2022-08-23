7ea5f252f61e4c43dda174a2ec10d680d27596d7
wuzhixin <zhixin.wu@cyclone-robotics.com>
2022-08-23 15:02:10 +0800

    feat:add utils

---
+++ packages/utils/vite.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from "vite";
+
+const path = require('path');
+export default defineConfig({
+
+    build:{
+        lib:{
+            entry: path.resolve(__dirname, 'lib/index.ts'),
+            name: 'fantasy-design-utils',
+            fileName: (format) => `fantasy-design-utils.${format}.js`,
+            formats:['es','umd','cjs']
+        },
+        rollupOptions:{
+            external:['react','react-dom'],
+            output:{
+                globals:{
+                    react:'React'
+                }
+            }
+        }
+    }
+})
