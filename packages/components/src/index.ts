4f16844a8d7c5fe215372c768efae12f0043a6c6
LuciferWZX <2396423791@qq.com>
2022-08-24 00:41:03 +0800

    fixed file

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -3,3 +3,4 @@ export {default as Button} from './button';
 export type { ButtonProps ,ButtonType} from './button';
 // export { default as SizeContext,SizeContextProvider} from './config-provider/SizeContext'
 export { default as ConfigProvider,ConfigContext} from './config-provider/ConfigContext'
+
